a6a29071e8181781eb80289d066cca4d54450a55
yudhapradiptar <yudha512000@gmail.com>
2021-04-01 11:58:18 +0700

    add list all pokemon

--- src/container/ListPokemon/ListPokemon.js
+++ src/container/ListPokemon/ListPokemon.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./ListPokemon.scss";
 import PokemonCard from "../../components/PokemonCard";
 import { useLocation } from "react-router-dom";
+import instance from "../../axios";
 
 const ListPokemon = () => {
   const location = useLocation();
@@ -9,14 +10,31 @@ const ListPokemon = () => {
   const arr = [2, 3, 5, 6, 12];
 
   useEffect(() => {
-    console.log(location);
-  }, [location]);
+    getData();
+  }, []);
+
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await getListPokemon();
+      console.log(response.data.results);
+
+      setData(response.data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getListPokemon = () => {
+    return instance.get("/pokemon");
+  };
 
   return (
     <>
       <div className="list-pokemon">
-        {arr.map((num) => (
-          <PokemonCard num />
+        {data.map((pokemon) => (
+          <PokemonCard name={pokemon.name} />
         ))}
       </div>
     </>
